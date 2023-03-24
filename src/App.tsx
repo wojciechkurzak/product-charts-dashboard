808b16864bacd0758ff2804d0e5d6b65d61fdc00
Wojciech Kurzak <wojciechkurzak@sitero.net>
2023-03-24 17:14:42 +0100

    Add navigation panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss'
 import Header from './components/header/Header'
+import Navigation from './components/navigation/Navigation'
 
 const App = () => {
   return (
     <div className='App'>
       <Header />
+      <Navigation />
     </div>
   )
 }
---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../../styles/navigation/Navigation.scss'
+
+const Navigation = () => {
+  return <nav></nav>
+}
+
+export default Navigation
