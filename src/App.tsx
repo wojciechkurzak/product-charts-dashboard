ea771a226188f0ee5614ba1b568d23664491b853
Wojciech Kurzak <wojciechkurzak@sitero.net>
2023-03-24 17:08:47 +0100

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.scss'
+import Header from './components/header/Header'
 
 const App = () => {
-  return <div className='App'>Hello world</div>
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import '../../styles/header/Header.scss'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        <a href='/'>Dashboard</a>
+      </h1>
+    </header>
+  )
+}
+
+export default Header
