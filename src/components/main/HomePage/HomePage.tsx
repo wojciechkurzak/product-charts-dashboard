918b162be6147cc37352e1de54a3df740b59ffe6
Wojciech Kurzak <wojciechkurzak@sitero.net>
2023-04-25 13:26:42 +0200

    Update home page

--- src/components/main/HomePage/HomePage.tsx
+++ src/components/main/HomePage/HomePage.tsx
@@ -3,7 +3,8 @@ import './HomePage.scss'
 const HomePage = () => {
   return (
     <div className='home-page'>
-      <p>Home page</p>
+      <p>Welcome</p>
+      <p>Create new cart and check prices before and after discount</p>
     </div>
   )
 }
